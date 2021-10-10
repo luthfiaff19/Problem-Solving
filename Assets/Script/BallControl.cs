29a2b7fbf4d27e21238b65f0e82a13b87b4faaee
Luthfi Alfikri <78073036+luthfiaff19@users.noreply.github.com>
2021-10-10 19:56:41 +0700

    step3-problem3

---
+++ Assets/Script/BallControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola supaya Bola bisa bergerak dengan bebas
+    private Rigidbody2D rigidBody2D;
+
+    // Titik asal lintasan bola
+    private Vector2 trajectoryOrigin;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    // Untuk mengakses informasi titik lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+
+    // Bola akan mengulang
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    // Bola bergerak dengan sendirinya, karena adanya PushBall
+    void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    // Memanggil Method ResetBall
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+
+    // Akan dipanggil sebelum pembaruan bingkai pertama dimulai
+    void Start()
+    {
+        trajectoryOrigin = transform.position;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        // Mulai game
+        RestartGame();
+    }
+}
