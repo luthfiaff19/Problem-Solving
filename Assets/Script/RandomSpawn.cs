30c396d66c38cdcf2670ff76481d2ef569d10a7f
Luthfi Alfikri <78073036+luthfiaff19@users.noreply.github.com>
2021-10-10 20:05:42 +0700

    step6-problem6

---
+++ Assets/Script/RandomSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+	public Transform[] Positions;
+	public GameObject Object;
+
+	public Transform Location;
+
+	public bool ToSpawn = true;
+
+	// Pembaruan akan dipanggil satu-persatu
+	void Update()
+	{
+		Location = Positions[Random.Range(0, Positions.Length)];
+
+		if (ToSpawn == true)
+		{
+			Instantiate(Object, Location);
+			ToSpawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+
+	// Setel acak posisi titik saat di run
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(0.75f);
+		ToSpawn = true;
+	}
+}
