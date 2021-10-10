2edb0382ebd354dd602e80af0ea822b2f3fc9364
Luthfi Alfikri <78073036+luthfiaff19@users.noreply.github.com>
2021-10-10 20:11:19 +0700

    step8-problem8

---
+++ Assets/Script/TouchMouse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMouse : MonoBehaviour
+{
+    private float speed = 4;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    // Pembaruan akan dipanggil satu-persatu
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            SetTargerPosiotion();
+        }
+        if(isMoving)
+        {
+            Move();
+        }
+    }
+
+    // Menyetel Satu Target Posisi
+    void SetTargerPosiotion()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+
+        isMoving = true;
+    }
+
+    // Menggerakan Bola Dengan Mouse
+    void Move()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if(transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+}
