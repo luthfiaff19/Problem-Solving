30621fa4b1af92b2d39eef40812db342b1611f2c
Luthfi Alfikri <78073036+luthfiaff19@users.noreply.github.com>
2021-10-10 20:13:35 +0700

    step9-problem9

---
+++ Assets/Script/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+     // Gerakan kanan kiri
+    private Rigidbody2D rb;
+    public float kecepatan;
+    public Vector2 gerakan;
+
+    // Akan dipanggil sebelum pembaruan bingkai pertama dimulai
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Pembaruan akan dipanggil satu-persatu
+    void Update()
+    {
+        // gerakan kanan kiri
+        gerakan.x = Input.GetAxisRaw("Horizontal");
+        gerakan.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    // Setelan gerakan kanan dan Kiri
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + gerakan * kecepatan * Time.fixedDeltaTime);
+    }
+}
+
