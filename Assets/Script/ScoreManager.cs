80f0e8a8098507ad31aad59822850aa912184c49
Luthfi Alfikri <78073036+luthfiaff19@users.noreply.github.com>
2021-10-10 20:18:33 +0700

    step10-problem10

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public Text text;
+    int score;
+
+    // Akan dipanggil sebelum pembaruan bingkai pertama dimulai
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Ketika titik box di sentuh maka box mengeluarkan score
+    public void ChangeScore(int boxValue)
+    {
+        score += boxValue;
+        text.text = "Box = " + score.ToString();
+    }
+}
